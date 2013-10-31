11827efe3cbea9e39b88002473df83c9c6f29371
Dmitrijs Ledkovs <xnox@debian.org>
2013-10-31 22:27:49 +0000

    Introduce compat config-freebsd.h header

---
+++ config-freebsd.h
@@ -0,0 +1,11 @@
+#define SIGPWR SIGUSR2
+
+#define CBAUD         0
+#define CBAUDEX       0
+
+#define PTRACE_TRACEME PT_TRACE_ME
+#define PTRACE_CONT PT_CONTINUE
+#define PTRACE_DETACH PT_DETACH
+
+#include <machine/param.h>
+#define CMSG_ALIGN(p) ALIGN(p)
